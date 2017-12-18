11528d27992330bf9fab17c195026255c819dd28
Wes <wes.coffay@gmail.com>
2017-12-18 10:47:38 -0500

    added install script, knife file, and key pem

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+current_dir = File.dirname(__FILE__)
+log_level                 :info
+log_location              STDOUT
+node_name                 "chefadmin"
+client_key                "#{current_dir}/chefadmin.pem"
+# change url to specificed server's FQDN
+chef_server_url           "https://ec2-54-147-131-34.compute-1.amazonaws.com/organizations/4thcoffee"
+cookbook_path             ["#{current_dir}/../cookbooks"]
+3. Verify your setup
